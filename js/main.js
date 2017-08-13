4742b7ffe57c9e8b95403f3d585d3417cd653780
sabihamahabubborna <30389227+sabihamahabubborna@users.noreply.github.com>
2017-08-13 15:13:08 +0600

    class 16

---
+++ js/main.js
@@ -0,0 +1,30 @@
+jQuery( document ).ready(function($) {
+ 
+   $('.portfolio-filter li').on("click", function(){
+
+   	var filterData = $(this).attr('data-filter');
+ $('.portfolio-list').isotope({
+ 	filter: filterData,
+ 	   
+
+ });
+ $('.portfolio-filter li').removeClass("active");
+ $(this).addClass("active");
+   });
+
+  $('.portfolio-list').isotope({
+  	 itemSelector: '.single-protfolio',
+ 	    percentPosition: true,
+ 	     masonry: {
+    // use outer width of grid-sizer for columnWidth
+    columnWidth: '.single-protfolio',
+    horizontalOrder: true,
+  }
+  });
+$('body').perfectScrollbar();
+$('.scrolled-another').perfectScrollbar({
+  theme: "yellow-theme"
+});
+
+ 
+});
